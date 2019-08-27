f7d14950bcc6f80a976716dc49af20075463adc1
Matt Mayo <matt_mayo@icloud.com>
2019-08-26 23:59:41 -0700

    Fix for name field not being passed

--- src/services/users/users.class.js
+++ src/services/users/users.class.js
@@ -4,7 +4,7 @@ const errors = require("@feathersjs/errors");
 exports.Users = class Users extends Service {
   create(data, params) {
     const email = data.email.toLowerCase();
-    const password = data.password;
+    const { name, password } = data;
 
     return this.find({
       query: { email }
@@ -16,6 +16,7 @@ exports.Users = class Users extends Service {
 
       const userData = {
         email,
+        name,
         password
       };
 
